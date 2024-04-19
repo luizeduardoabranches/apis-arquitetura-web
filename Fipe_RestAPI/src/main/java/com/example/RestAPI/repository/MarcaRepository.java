de2e64923a0bef761eb6d796fb62ff3a2ae145af
Luiz Eduardo Rangel da Silva Abranches <luizeduardo.abranches@gmail.com>
2024-04-19 20:04:54 -0300

    Delete Fipe_RestAPI directory

--- Fipe_RestAPI/src/main/java/com/example/RestAPI/repository/MarcaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.RestAPI.repository;
-
-import com.example.RestAPI.Model.MarcasEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MarcaRepository extends MongoRepository<MarcasEntity, String> {
-}
